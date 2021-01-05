6f226592b5b6aa3403bd1f6be54a42f4a25c7445
natbaby106 <ndtaylor@ucsd.edu>
2021-01-04 22:11:45 -0800

    created form component and added it to main screen

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
     const classes = useStyles();
@@ -16,7 +17,7 @@ const Main = () => {
                     Try saying: Add income for $100in Category Salary for Monday ...
                 </Typography>
                 <Divider />
-                {/* Form */}
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
