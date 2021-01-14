f9d7a3725fce803850f3d9550d53e6d81fc8a448
natbaby106 <ndtaylor@ucsd.edu>
2021-01-13 23:58:33 -0800

    created add transaction function

--- src/context/context.js
+++ src/context/context.js
@@ -9,8 +9,13 @@ export const ExpenseTrackerContext = createContext(initialState);
 export const Provider = ({ children }) => {
     const [transactions, dispatch] = useReducer(contextReducer, initialState);
 
+    // Action Creators 
     const deleteTransactions = (id) => {
         dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+    };
+
+    const addTransaction = (transaction) => {
+        dispatch({type : 'ADD_TRANSACTION'});
     };
 
     return (
