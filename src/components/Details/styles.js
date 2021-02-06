80abda865701874fdee7d640debc4a12db334ae8
natbaby106 <ndtaylor@ucsd.edu>
2021-02-05 22:47:51 -0800

    styles padding for list js

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -3,8 +3,10 @@ import { makeStyles } from '@material-ui/core';
 export default makeStyles (() => ({
     income: {
         borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
+        padding: "10px",
     },
     expense: {
         borderBottom: '10px solid rgba(255, 0, 0.5)',
+        padding: "10px",
     }
 }));
