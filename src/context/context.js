214664521616365b76abe7db49ea877319378921
natbaby106 <ndtaylor@ucsd.edu>
2021-02-06 18:57:34 -0800

    adding logs in context

--- src/context/context.js
+++ src/context/context.js
@@ -9,6 +9,8 @@ export const ExpenseTrackerContext = createContext(initialState);
 export const Provider = ({ children }) => {
     const [transactions, dispatch] = useReducer(contextReducer, initialState);
 
+    console.log(transactions);
+
     // Action Creators 
     const deleteTransaction = (id) => dispatch({ type: 'DELETE_TRANSACTION', payload: id });
     const addTransaction = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
