b97e10f733c7521e1d9c5cd880793eb83dd883a6
natbaby106 <ndtaylor@ucsd.edu>
2021-02-10 19:42:57 -0800

    updated styles for form

--- src/components/Main/Form/styles.js
+++ src/components/Main/Form/styles.js
@@ -5,9 +5,12 @@ export default makeStyles(() => ({
     display: 'flex',
     justifyContent: 'center',
     marginBottom: '-10px',
+    padding: '5px'
   },
   button: {
     marginTop: '20px',
     padding: 0,
+    margin: '5px'
+
   },
 }));
