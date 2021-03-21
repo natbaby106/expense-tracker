c4969700e00a570320df78765c31fdcc3d0c9228
natbaby106 <ndtaylor@ucsd.edu>
2021-03-20 19:36:50 -0700

    added delete css

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -21,5 +21,12 @@ export default makeStyles (() => ({
         padding: "10px",
         backgroundColor: "red",
         marginTop: '5px'
+    },
+    delet: {
+        maxHeight: '450px',
+        borderBottom: '10px solid rgba(255, 0, 0.5)',
+        padding: "15px",
+        backgroundColor: "red",
+        marginTop: '5px'
     }
 }));
