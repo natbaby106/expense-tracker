882524061691ffe69e426ce720eea81c96613b5d
natbaby106 <ndtaylor@ucsd.edu>
2021-03-21 21:34:42 -0700

    added delete styling to form

--- src/components/Main/Form/styles.js
+++ src/components/Main/Form/styles.js
@@ -22,5 +22,12 @@ export default makeStyles(() => ({
         borderBottom: '10px solid rgba(255, 0, 0.5)',
         padding: "10px",
         backgroundColor: "red",
+    },
+    delet: {
+        maxHeight: '450px',
+        borderBottom: '10px solid rgba(255, 0, 0.5)',
+        padding: "15px",
+        backgroundColor: "red",
+        marginTop: '5px'
     }
 }));
